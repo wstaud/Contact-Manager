4b661a1dfc79c71781a79dda553d0022dda203fa
xconstaud <xconstaud@gmail.com>
2017-03-23 15:26:51 -0500

    cleanup overall code

--- contact_manager.php
+++ contact_manager.php
@@ -1,12 +1,12 @@
 <?php
-// Shows into and calls prompt()
+// Shows intro and calls prompt() which prompts user for input
 function startup() {
     echo "Contacts Manager" . PHP_EOL;
     echo "----------------------------" . PHP_EOL;
     prompt();
 }
 
-// Asks user for input
+// Asks user for input (view contacts, search, add, delete or exit)
 function prompt() {
     fwrite(STDOUT, PHP_EOL . "1: View Contacts" . PHP_EOL .  "2: Search Contacts" . PHP_EOL . "3: Add Contact" . PHP_EOL . "4: Delete Contact" . PHP_EOL . "0: Exit" . PHP_EOL);
     $answer = trim(fgets(STDIN));
@@ -33,19 +33,22 @@ function prompt() {
     }
 }
 
-// Displays all the contacts in a human friendly formant. 
+// Displays all the contacts in a human friendly formant.  
 function displayContacts() {
+    // call clearstatcache() in order to update file in cache in case user added or deleted a contact
     clearstatcache();
     echo "============" . PHP_EOL;
+
     $filename = "contacts.txt";
     $handle = fopen($filename, "r");
     $contents = fread($handle, filesize($filename));
     $contents = trim($contents);
     fclose($handle);
-    //echo $contents . PHP_EOL;
     $contactsArray = explode("\n", $contents);
 
-    echo "Name        | Phone Number" . PHP_EOL;
+    echo "Name      | Phone Number" . PHP_EOL;
+    
+    //Breaks apart the contacts further so that phone numbers may be formated to be human friendly. Calls formatNumber() to achieve this 
     foreach($contactsArray as $key => $contact) {
         $tempArray = explode("|", $contact);
         $result = array("name"  =>   $tempArray[0],
@@ -75,7 +78,7 @@ function addContact() {
 
 }
 
-// Removes contact from contacts. Will use strict search to ensure user deletes right person. Rewrites the entire contacts file.
+// Removes contact from contacts. Will use strict search to ensure user deletes right person. Rewrites the entire contacts file to prevent gaps.
 function deleteContact() {
     $filename = "contacts.txt";
     $handle = fopen($filename, "r");
@@ -113,6 +116,7 @@ function deleteContact() {
 }
 
 // Allows user to search all contacts that fit loosly within their search results. It's case insensitive and can search partial strings
+// Users can search full, first or last name... partially. Ex. "wil" may return William Staud
 function searchContact() {
     clearstatcache();
     $filename = "contacts.txt";
@@ -139,7 +143,7 @@ function searchContact() {
     prompt();
 }
 
-
+// Takes standard phone number and adds dashes to them
 function formatNumber($number) {
     $num1 = substr($number, 0, 3); 
     $num2 = substr($number, 3, 3); 
@@ -152,6 +156,5 @@ function formatNumber($number) {
 
 // Call startup() on program launch
 startup();
-
 
 ?>
